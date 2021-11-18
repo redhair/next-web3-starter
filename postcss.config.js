094e1553ba7bade30b488f59525f23f5823de9dc
redhair <thomasbonanni@afito.com>
2021-11-18 11:52:27 -0500

    chore(tailwind): added tailwindcss + postcss + purgecss

---
+++ postcss.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: [
+    'tailwindcss',
+    'postcss-preset-env',
+    [
+      '@fullhuman/postcss-purgecss',
+      {
+        content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
+        defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+      },
+    ],
+  ],
+};
