fcf4032b87659ac312b22f25583d48828534f370
redhair <thomasbonanni@afito.com>
2021-11-18 12:21:35 -0500

    chore: tailwind config update + removed next boilerplate

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,6 @@
 module.exports = {
-  plugins: [
-    'tailwindcss',
-    'postcss-preset-env',
-    [
-      '@fullhuman/postcss-purgecss',
-      {
-        content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
-        defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-      },
-    ],
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
